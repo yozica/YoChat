dc1ac76ddf75b56bda47dbdf9445409c8a4c8535
yozica <lichengmingisme@foxmail.com>
2024-10-05 13:01:10 +0800

    feat: 添加侧边抽屉功能

--- src/views/chatroom/components/ChatHeader.vue
+++ src/views/chatroom/components/ChatHeader.vue
@@ -1,4 +1,9 @@
 <script lang="ts" setup>
+import { ref } from "vue";
+import { NButton, NIcon } from "naive-ui";
+import { IosMore } from "@vicons/ionicons4";
+import DrawerPanel from "./DrawerPanel.vue";
+
 withDefaults(
   defineProps<{
     roomNumber: string;
@@ -7,13 +12,24 @@ withDefaults(
     roomNumber: "正在加载房间信息"
   }
 );
+
+const show = ref(false);
 </script>
 
 <template>
   <div class="chat-header">
     <div class="header-left"></div>
     <div class="title">{{ roomNumber }}</div>
-    <div class="header-right"></div>
+    <div class="header-right">
+      <div class="header-tools">
+        <n-button text @click="show = true">
+          <n-icon size="30">
+            <IosMore />
+          </n-icon>
+        </n-button>
+      </div>
+    </div>
+    <DrawerPanel v-model:show="show" :roomNumber="roomNumber" />
   </div>
 </template>
 
@@ -29,14 +45,27 @@ withDefaults(
 
   .header-left,
   .header-right {
-    width: 30px;
-    height: 30px;
+    width: 0;
+    height: 40px;
   }
 
   .header-left {
     display: flex;
     align-items: center;
     justify-content: center;
+  }
+
+  .header-right {
+    position: relative;
+
+    .header-tools {
+      position: absolute;
+      top: 0;
+      right: 0;
+      display: flex;
+      align-items: center;
+      height: 40px;
+    }
   }
 
   .title {
--- src/views/chatroom/components/DrawerPanel.vue
+++ src/views/chatroom/components/DrawerPanel.vue
@@ -1,11 +1,56 @@
 <script lang="ts" setup>
+import { ref, onUnmounted } from "vue";
+import { NDrawer, NDrawerContent, NList, NListItem, NThing, NSpace, NTag } from "naive-ui";
+import { useChatroomStore } from "@/store";
+
+const chatroomStore = useChatroomStore();
+
 const show = defineModel<boolean>("show");
+
+withDefaults(
+  defineProps<{
+    roomNumber: string;
+  }>(),
+  {
+    roomNumber: "正在加载房间信息"
+  }
+);
+
+const drawerWidth = ref<500 | "100vw">(500);
+
+const resizeObserver = new ResizeObserver((entry) => {
+  console.log(entry[0].contentRect.width);
+  if (entry[0].contentRect.width <= 500) {
+    drawerWidth.value = "100vw";
+  } else {
+    drawerWidth.value = 500;
+  }
+});
+resizeObserver.observe(document.body);
+
+onUnmounted(() => {
+  resizeObserver.disconnect();
+});
 </script>
 
 <template>
-  <n-drawer v-model:show="show" :width="502">
-    <n-drawer-content title="斯通纳" closable>
-      《斯通纳》是美国作家约翰·威廉姆斯在 1965 年出版的小说。
+  <n-drawer
+    v-model:show="show"
+    :width="drawerWidth"
+    :style="drawerWidth === 500 ? '' : '--n-border-radius: 0'"
+  >
+    <n-drawer-content :title="roomNumber" closable>
+      <n-list>
+        <n-list-item>
+          <n-thing title="房间成员">
+            <n-space>
+              <n-tag v-for="(item, index) in chatroomStore.roomMembers" :key="index">
+                {{ item }}
+              </n-tag>
+            </n-space>
+          </n-thing>
+        </n-list-item>
+      </n-list>
     </n-drawer-content>
   </n-drawer>
 </template>
